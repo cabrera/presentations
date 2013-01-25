93d3d9a619cba333b23d3f2fb137b162155df311
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-01-25 11:08:57 -0500

    feat(pytalk): color urls.

--- python-for-programmers/tests.py
+++ python-for-programmers/tests.py
@@ -1,4 +1,4 @@
 import subprocess
 
 def test_build_talk():
-    subprocess.call('pandoc -t beamer -V theme:Warsaw talk.md -o ../talk.pdf'.split())
+    subprocess.call('pandoc -t beamer -V theme:Warsaw --template=my.beamer talk.md -o ../talk.pdf'.split())
